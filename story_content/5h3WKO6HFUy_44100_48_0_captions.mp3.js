23e1989908c799c6b00295784f08401e9d53f42e
nicoleDS <57958857+nicoleDS@users.noreply.github.com>
2020-02-18 14:39:00 -0500

    v1

---
+++ story_content/5h3WKO6HFUy_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.36.21289.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:06.034%0D%0AThe%20online%20resource%20loads%20in%20a%20new%20tab,%20where%20you%20can%20view%20additional%20details%20and%20access%20the%20full%20text.%0D%0A%0D%0A"}')
---
+++ story_content/5t4G9b6JJpk_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.36.21289.0%0D%0A%0D%0A00:00:00.102%20--%3E%2000:00:03.109%0D%0ALet%E2%80%99s%20select%20the%20database%20Academic%20Search%20Complete.%0D%0A%0D%0A"}')
