dabe7092f98296e63113bbd8eef64c16f71faf2d
nicoleDS <nicole.desarno@temple.edu>
2024-05-30 13:01:34 -0400

    Update to BusDatabases

--- BusDatabases/story_content/user.js
+++ BusDatabases/story_content/user.js
@@ -1,1 +1,8 @@
-// User defined code goes here
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
