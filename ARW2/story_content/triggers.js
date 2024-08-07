64f24976adf3c1663fe2367186e238f230c217d2
nicoleDS <nicole.desarno@temple.edu>
2024-08-07 10:33:18 -0400

    updated to 802 mod 2

---
+++ ARW2/story_content/triggers.js
@@ -0,0 +1,24 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6IPJKNoCp2i":
+        Script1();
+        break;
+      case "63pBTA9NliC":
+        Script2();
+        break;
+      case "6ioWUxgBwxr":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
--- ARW2/story_content/user.js
+++ ARW2/story_content/user.js
@@ -1,7 +1,66 @@
-function ExecuteScript(strId)
+window.InitUserScripts = function()
 {
-  switch (strId)
-  {
-  }
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+script.type = 'text/javascript';
+head.appendChild(script)
 }
 
+window.Script2 = function()
+{
+  var player = GetPlayer();
+ 
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwWjakAOrCy9h2RHn5c1FKgzl_A4mmDueFuo9Jqm7q6kW61rDgYXdYIfDftUlDlpHk-/exec";
+
+storyline =
+{
+ "date" : new Date().toJSON().slice(0,10), //STORE DATE
+ "TimeElapsed" : player.GetVar("TimeElapsed"),
+ "Recap" : player.GetVar("Recap"),
+ "recap_Timeline" : player.GetVar("recap_Timeline"),
+ "recap_Sources" : player.GetVar("recap_Sources"),
+ "recap_Databases" : player.GetVar("recap_Databases"),
+ "topic_KC" : player.GetVar("topic_KC"),
+ "database_KC" : player.GetVar("database_KC"),
+ "GeneralTopicTextEntry1" : player.GetVar("GeneralTopicTextEntry1"),
+ "AspectsTopicTextEntry" : player.GetVar("AspectsTopicTextEntry"),
+ "WhoTextEntry" : player.GetVar("WhoTextEntry"),
+ "WhenTextEntry" : player.GetVar("WhenTextEntry"),
+ "WhereTextEntry" : player.GetVar("WhereTextEntry"),
+ "blank_Activity" : player.GetVar("blank_Activity"),
+ "reset_Activity" : player.GetVar("reset_Activity")
+}
+
+}
+
+window.Script3 = function()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+};
