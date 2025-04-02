4e9cb5849f1d15eb7179b48ec04b849de2ef14f9
nicoleDS <nicole.desarno@temple.edu>
2025-04-02 13:39:28 -0400

    New testing folder and tutorial

---
+++ Test/CC1/story_content/triggers.js
@@ -0,0 +1,82 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Va6eAtwnCh":
+        Script1();
+        break;
+      case "6eZBj6bHWag":
+        Script2();
+        break;
+      case "6OzdDCysVpV":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6kp2GxYsH0N');
+const duration = 750;
+const easing = 'ease-out';
+const id = '626LyAQARfn';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5Xvawtarfyf');
+const duration = 750;
+const easing = 'ease-out';
+const id = '626LyAQARfn';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
---
+++ Test/CC1/story_content/user.js
@@ -0,0 +1,31 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script3 = function()
+{
+  let currentTime = new Date();
+let month = currentTime.getMonth() + 1;
+let day = currentTime.getDate();
+let year = currentTime.getFullYear();
+
+// Putting it together
+let dateString = month + "/" + day + "/" + year;
+
+//Pushing data to Storyline
+let player = GetPlayer();
+player.SetVar("todaysDate", dateString);
+}
+
+};
