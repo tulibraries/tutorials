9198c429070bc90e55216687e171f774d3a38dda
nicoleDS <nicole.desarno@temple.edu>
2025-08-12 15:43:29 -0400

    intro sources update

--- IntroSources/story_content/triggers.js
+++ IntroSources/story_content/triggers.js
@@ -2,6 +2,18 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "62Jxikj6oR1":
+        Script1();
+        break;
+      case "6AmKFSIjKDM":
+        Script2();
+        break;
+      case "5bkaUMgW5Eb":
+        Script3();
+        break;
+      case "5xaAoVmNq8C":
+        Script4();
+        break;
   }
 }
 
@@ -9,7 +21,15 @@ window.InitExecuteScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
+var once = player.once;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
 };
