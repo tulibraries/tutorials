15beec1335169f339d973cc1210bedbbfba34b88
nicoleDS <nicole.desarno@temple.edu>
2025-08-13 15:39:44 -0400

    research process update

---
+++ ResearchProcess/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6TdTpf8L3s8":
+        Script1();
+        break;
+      case "5x89nycTMTx":
+        Script2();
+        break;
+      case "6DLk6p95DrD":
+        Script3();
+        break;
+      case "5r9HbsRxK3i":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
