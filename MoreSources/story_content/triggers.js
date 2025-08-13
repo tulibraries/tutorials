b86ed46bd6e0d91298b0d532fdaf0236cde74852
nicoleDS <nicole.desarno@temple.edu>
2025-08-13 13:56:06 -0400

    More Sources update

--- MoreSources/story_content/triggers.js
+++ MoreSources/story_content/triggers.js
@@ -2,6 +2,18 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6m1iPQAKxvi":
+        Script1();
+        break;
+      case "6n10v4X1Vfc":
+        Script2();
+        break;
+      case "5gcPgjSp1UH":
+        Script3();
+        break;
+      case "66aqqVZ0lp4":
+        Script4();
+        break;
   }
 }
 
@@ -9,7 +21,15 @@ window.InitExecuteScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
+var once = player.once;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
 };
--- MoreSources/story_content/user.js
+++ MoreSources/story_content/user.js
@@ -2,7 +2,79 @@ window.InitUserScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
+var once = player.once;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  let currentTime = new Date();
+let month = currentTime.getMonth() + 1;
+let day = currentTime.getDate();
+let year = currentTime.getFullYear();
+
+// Putting it together
+let dateString = month + "/" + day + "/" + year;
+
+//Pushing data to Storyline
+let player = GetPlayer();
+player.SetVar("todaysDate", dateString);
+}
+
+window.Script2 = function()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+window.Script3 = function()
+{
+  var player = GetPlayer();
+ 
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzPRY1FWk8g6xm_Ltj6a4ny_bmAUaQkfY32kGR33LHuKBSyVSA5tRsUADeAe1f3DKWMVw/exec";
+
+storyline =
+{
+ "date" : new Date().toJSON().slice(0,10), //STORE DATE
+ "TimeElapsed" : player.GetVar("TimeElapsed"),
+ "TutorialName" : player.GetVar("TutorialName"),
+ "CourseNumber" : player.GetVar("CourseNumber"),
+ "KC1" : player.GetVar("KC1"),
+ "KC2" : player.GetVar("KC2")
+}
+
+}
+
+window.Script4 = function()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
 };
