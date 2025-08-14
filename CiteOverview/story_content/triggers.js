0f298440133e1baccd3679e86ee286b89e2d9a26
nicoleDS <nicole.desarno@temple.edu>
2025-08-14 11:11:47 -0400

    citations overview update

--- CiteOverview/story_content/triggers.js
+++ CiteOverview/story_content/triggers.js
@@ -2,6 +2,18 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5clt6qduH4b":
+        Script1();
+        break;
+      case "6q2aEGjw0Vm":
+        Script2();
+        break;
+      case "68vc2uFVPou":
+        Script3();
+        break;
+      case "5tPec0m4GXh":
+        Script4();
+        break;
   }
 }
 
@@ -9,7 +21,15 @@ window.InitExecuteScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
+var once = player.once;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
 };
