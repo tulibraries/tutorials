883853ed420e2ae53b74ff7b176e08c5bc9bd7e7
nicoleDS <nicole.desarno@temple.edu>
2025-08-15 15:28:09 -0400

    database basics update

---
+++ DatabaseBasics/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Wh3fU1r7lH":
+        Script1();
+        break;
+      case "5pWuADgrSEi":
+        Script2();
+        break;
+      case "5Vva03WtsBI":
+        Script3();
+        break;
+      case "6dLJ3DltKGj":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
