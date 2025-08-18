f1d79bc7ac17bee7c7ea5bd25a7b4393b4d00d8a
nicoleDS <nicole.desarno@temple.edu>
2025-08-18 10:45:45 -0400

    sift update

---
+++ SIFTmisinfo/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5vZFFg6GMwx":
+        Script1();
+        break;
+      case "6n13dvfTh8M":
+        Script2();
+        break;
+      case "6RIVvlJtPY0":
+        Script3();
+        break;
+      case "6BsrdQfXcGi":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
