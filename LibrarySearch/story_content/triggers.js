eb10446e160e39bf35182a8f7dae78d05fb3ab98
nicoleDS <nicole.desarno@temple.edu>
2025-08-25 13:09:37 -0400

    using library website update

---
+++ LibrarySearch/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qwGXmFWcho":
+        Script1();
+        break;
+      case "5UtFtqfBOjb":
+        Script2();
+        break;
+      case "5qGq5PiG53P":
+        Script3();
+        break;
+      case "6NaBqWO9q9w":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
