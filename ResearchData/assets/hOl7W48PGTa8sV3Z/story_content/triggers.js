b191416d05c119b9bbc3ede7b58ac261b0ce6a92
nicoleDS <nicole.desarno@temple.edu>
2026-07-14 10:01:13 -0400

    New tutorial
    What is Research Data

---
+++ ResearchData/assets/hOl7W48PGTa8sV3Z/story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Wujk6URpOA":
+        Script1();
+        break;
+      case "6FSAQIO4U1d":
+        Script2();
+        break;
+      case "6fPpLXYBQFo":
+        Script3();
+        break;
+      case "5ihvxWxwBOV":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
