10fc09526bb666e6de3d859e0935016ed6a13958
nicoleDS <nicole.desarno@temple.edu>
2026-09-08 14:47:22 -0400

    APA update

---
+++ CiteAPA/story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Yi2eWmMaAS":
+        Script1();
+        break;
+      case "6jO3g1qvBxL":
+        Script2();
+        break;
+      case "6GKx8OCEFUp":
+        Script3();
+        break;
+      case "6hAD54meyyZ":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
